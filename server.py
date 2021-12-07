425914a12e91abcc4b81091165abf03280eb8a11
Pal Pentesting <75971101+pal-development@users.noreply.github.com>
2021-12-07 22:16:21 +1100

    flipped around with client

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import date
+import os
 
 clients_ip = "" ## client to connects ip address (public)
 today = date.today()
@@ -9,7 +10,7 @@ class Infinite:
     def Create():
         for i in range(50):
             date =  today.strftime("-%d-%m-%Y-%H-%M-%S")
-            print(requests.get(f"https://ppng.io/{clients_ip}{str(date)}"))
+            os.system(f"echo 'pc_on_{date}' | curl -T - https://ppng.io/{clients_ip}{date}")
 
 
 if __name__ == "__main__":
