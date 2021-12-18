44021391094e189953c9b07afd0bee3afc751689
BeCyber/Pal <75971101+pal-development@users.noreply.github.com>
2021-12-18 23:09:03 +1100

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,12 @@
 import requests
-from datetime import date
 
-
-today = date.today()
 client_ip = requests.get("https://api.ipify.org").text
 
 class Infinite:
 
     def Create():
-        for i in range(50):
-            date =  today.strftime("-%d-%m-%Y-%H-%M-%S")
-            request = requests.get(f"https://ppng.io/{client_ip}{str(date)}")
+        while True:
+            request = requests.get(f"https://ppng.io/{client_ip}-infinite")
             # do something will the request
 
 if __name__ == "__main__":
